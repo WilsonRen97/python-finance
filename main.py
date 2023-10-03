259bc9f5a76ca637adf4be4e479bc4015eb0fe3a
Wilson Ren <wilson@fakeemail.com>
2023-10-03 10:53:00 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,197 @@
+from flask import Flask, render_template, request, redirect, g
+import requests
+import math
+import sqlite3
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('agg')
+
+
+# Create Flask App
+app = Flask(__name__)
+database = "datafile.db"
+
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = sqlite3.connect(database)
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+@app.route("/")
+def home():
+    conn = get_db()
+    cursor = conn.cursor()
+    result = cursor.execute("select * from cash")
+    cash_result = result.fetchall()
+    # 計算台幣與美金總額
+    taiwanese_dollars = 0
+    us_dollars = 0
+    for data in cash_result:
+        taiwanese_dollars += data[1]
+        us_dollars += data[2]
+    # 獲取匯率資訊
+    r = requests.get('https://tw.rter.info/capi.php')
+    currency = r.json()
+    total = math.floor(taiwanese_dollars + us_dollars *
+                       currency["USDTWD"]["Exrate"])
+
+    # 取得所有股票的資訊
+    result2 = cursor.execute("select * from stock")
+    stock_result = result2.fetchall()
+    unique_stock_list = []
+    # 找到所有股票的代號
+    for data in stock_result:
+        if data[1] not in unique_stock_list:
+            unique_stock_list.append(data[1])
+    # 計算購買總成本
+    total_cost = 0
+    for d in stock_result:
+        total_cost += d[2] * d[3] + d[4] + d[5]
+    # 計算總市值
+    total_stock_value = 0
+
+    # 取得單一股票資訊
+    stock_info = []
+    today = datetime.now().date()
+    formatted_date = today.strftime("%Y%m%d")
+    for stock in unique_stock_list:
+        result = cursor.execute(
+            "select * from stock where stock_id =?", (stock, ))
+        result = result.fetchall()
+        stock_cost = 0  # 單一股票總花費
+        shares = 0  # 總股數
+        for d in result:
+            shares += d[2]
+            stock_cost += int(d[2] * d[3] + d[4] + d[5])
+        # 取得今日收盤價
+        url = "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&stockNo=" + stock
+        response = requests.get(url)
+        data = response.json()
+        price_array = data['data']
+        current_price = float(price_array[len(price_array) - 1][6])
+        # 單一股票總市值
+        total_value = int(current_price * shares)
+        total_stock_value += total_value
+        # 單一股票的平均成本
+        average_cost = round(stock_cost / shares, 2)
+        # 單一股票報酬率
+        rate_of_return = round(
+            (total_value - stock_cost) * 100 / stock_cost, 2)
+        stock_info.append({"stock_id": stock, "stock_cost": stock_cost, "total_value": total_value, "average_cost": average_cost,
+                           "shares": shares, "current_price": current_price, 'rate_of_return': rate_of_return})
+
+    # 計算單一股票的資產佔比
+    for stock in stock_info:
+        stock['value_percentage'] = round(
+            stock['total_value'] * 100 / total_stock_value, 2)
+    data = {'total': total, 'cash_result': cash_result, 'td': taiwanese_dollars,
+            'ud': us_dollars, 'currency': currency["USDTWD"]["Exrate"], 'stock_info': stock_info}
+
+    # 繪製股票圓餅圖
+    if len(unique_stock_list) != 0:
+        labels = tuple(unique_stock_list)
+        sizes = [d['total_value'] for d in stock_info]
+        fig, ax = plt.subplots(figsize=(6, 5))
+        print(sizes)
+        ax.pie(sizes, labels=labels, autopct=None, shadow=False)
+        fig.subplots_adjust(top=1, bottom=0, right=1,
+                            left=0, hspace=0, wspace=0)
+        plt.savefig('static/piechart.jpg', dpi=200)
+
+    # 繪製股票、現金圓餅圖
+    if us_dollars != 0 or taiwanese_dollars != 0 or total_stock_value != 0:
+        labels = ('USD', 'TWD', 'Stock')
+        sizes = (us_dollars * currency["USDTWD"]["Exrate"],
+                taiwanese_dollars, total_stock_value)
+        print(sizes)
+        fig, ax = plt.subplots(figsize=(6, 5))
+        ax.pie(sizes, labels=labels, autopct=None, shadow=False)
+        fig.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+        plt.savefig('static/piechart2.jpg', dpi=200)
+
+    return render_template('index.html', data=data)
+
+
+@app.route("/cash")
+def cash_form():
+    return render_template('cash.html')
+
+
+@app.route("/cash", methods=['POST'])
+def submit_cash():
+    # 取得金額與日期資料
+    if request.values['taiwanese-dollars'] != '':
+        taiwanese_dollars = request.values['taiwanese-dollars']
+    else:
+        taiwanese_dollars = 0
+    if request.values['us-dollars'] != '':
+        us_dollars = request.values['us-dollars']
+    else:
+        us_dollars = 0
+    note = request.values['note']
+    date = request.values['date']
+
+    # 更新數據庫資料
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute(
+        """insert into cash (taiwanese_dollars, us_dollars, note, date_info) values (?, ?, ?, ?)""", (taiwanese_dollars, us_dollars, note, date))
+    conn.commit()
+    # 將使用者導向回到主頁面
+    return redirect("/")
+
+
+@app.route("/cash-delete", methods=['POST'])
+def cach_delete():
+    id = request.values['id']
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("""delete from cash where transaction_id=?""", (id,))
+    conn.commit()
+    return redirect("/")
+
+
+@app.route("/stock")
+def stock_form():
+    return render_template('stock.html')
+
+
+@app.route("/stock", methods=['POST'])
+def submit_stock():
+    # 取得金額與日期資料
+    stock_id = request.values['stock-id']
+    stock_num = request.values['stock-num']
+    stock_price = request.values['stock-price']
+    if request.values['processing-fee'] != '':
+        processing_fee = request.values['processing-fee']
+    else:
+        processing_fee = 0
+    if request.values['tax'] != '':
+        tax = request.values['tax']
+    else:
+        tax = 0
+    date = request.values['date']
+
+    # 更新數據庫資料
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute(
+        """insert into stock (stock_id, stock_num, stock_price, processing_fee, tax, date_info) values (?, ?, ?, ?, ?, ?)""", (stock_id, stock_num, stock_price, processing_fee, tax, date))
+    conn.commit()
+    # 將使用者導向回到主頁面
+    return redirect("/")
+
+
+# @app.route('/hello/<name>')
+# def home(name=None):  # giving a default argument
+#     return render_template('index.html', name=name)
+if __name__ == '__main__':
+    app.run(debug=True)
